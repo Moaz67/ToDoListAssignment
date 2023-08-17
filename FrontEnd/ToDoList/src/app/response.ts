9d69b1eea0e5c5ece0f14746d2a1884b8b8203e1
moaz67 <moaz5652@gmail.com>
2023-08-17 18:51:57 +0500

    Added comment in reponse file

--- FrontEnd/ToDoList/src/app/response.ts
+++ FrontEnd/ToDoList/src/app/response.ts
@@ -1,6 +1,6 @@
 import { Task1 } from "./task1";
 import { Taskstatus } from "./taskstatus";
-
+// this class is to get all the data from the backend
 export class Response {
     items:Task1[]=[]
     taskstatus:Taskstatus[]=[]
